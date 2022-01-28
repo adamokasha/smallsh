3a65969bb464e4495a0e05c1dadb16763114962b
Adam Okasha <okashaa@oregonstate.edu>
2022-01-27 19:31:11 -0800

    Add isComment and isEmpty

--- smallsh.c
+++ smallsh.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-struct userInputs {
+struct UserInput
+{
   char *command;
   char args[512];
   char *inputRedirect;
@@ -15,18 +17,80 @@ struct userInputs {
   bool isBackgroundProcess;
 };
 
-void prompt(char **buf, size_t *buflen) {
+/*
+  Removes a trailing '\n' from character string
+*/
+void cleanTrailingNewlineFromString(char *str)
+{
+  char *newLinePtr = str + (strlen(str) - 1);
+
+  if (*newLinePtr == '\n')
+  {
+    *newLinePtr = '\0';
+  }
+
+  newLinePtr = NULL;
+}
+
+void prompt(char **buf, size_t *buflen)
+{
   printf(": ");
   getline(buf, buflen, stdin);
 }
 
-int main() {
+bool isComment(char *buf)
+{
+  if (buf[0] == '#')
+    return true;
+  return false;
+}
+
+bool isEmptyString(char *buf)
+{
+  char *bufCopy = calloc(strlen(buf) + 1, sizeof(char));
+  strcpy(bufCopy, buf);
+  char *token = NULL;
+  char *savePtr = NULL;
+
+  token = strtok_r(bufCopy, " ", &savePtr);
+
+  if (token == NULL) {
+    free(bufCopy);
+    bufCopy = NULL;
+    // TODO: remove
+    printf("TOKEN IS NULL\n");
+    return true;
+  }
+
+  free(bufCopy);
+  bufCopy = NULL;
+
+  return false;
+}
+
+void parseUserInput(char *buf)
+{
+  cleanTrailingNewlineFromString(buf);
+
+  if (isComment(buf) || isEmptyString(buf))
+  {
+    // TODO: remove
+    printf("Is comment or empty string.\n");
+    return;
+  }
+}
+
+int main()
+{
   char *buf = NULL;
   size_t buflen;
 
   prompt(&buf, &buflen);
-  printf(buf);
+  parseUserInput(buf);
   fflush(stdin);
+
+  free(buf);
+  buf = NULL;
 
   return EXIT_SUCCESS;
 }
