4e4fc85e57430756145a00a90dc7078f82650b8a
Adam Okasha <okashaa@oregonstate.edu>
2022-01-31 17:15:23 -0800

    Update smallshfgspawn to use redirection

--- smallshfgspawn.c
+++ smallshfgspawn.c
@@ -4,6 +4,7 @@
 #include <stdio.h>    // for printf and perror
 #include <stdlib.h>   // for exit
 #include <unistd.h>   // for execv, getpid, fork
+#include <fcntl.h>
 
 #include "smallshtok.h"
 #include "smallshstatus.h"
@@ -22,6 +23,27 @@ void spawnForegroundProcess(struct UserInput *userInput, struct CommandStatus *c
     // exit(1);
     break;
   case 0:
+    if (userInput->inputFile != NULL)
+    {
+      int sourceFD = open(userInput->inputFile, O_RDONLY);
+      int result = dup2(sourceFD, 0);
+      if (result == -1)
+      {
+        perror("source dup2()");
+        exit(1);
+      }
+    }
+    if (userInput->outputFile != NULL)
+    {
+      int targetFD = open(userInput->outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0740);
+      int result2 = dup2(targetFD, 1);
+      printf("DESC %d", targetFD);
+      if (result2 == -1)
+      {
+        perror("target dup2()");
+        exit(2);
+      }
+    }
     execvp(userInput->command, userInput->args);
     perror("execvp");
     exit(1);
